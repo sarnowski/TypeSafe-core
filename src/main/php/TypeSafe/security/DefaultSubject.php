e09d5770a16072f048f65f5478a83cd48e724fa6
Tobias Sarnowski <sarnowski@cosmocode.de>
2010-09-17 14:23:15 +0200

    for consistent naming, the source folder is renamed to correct camelcase

--- src/main/php/TypeSafe/security/DefaultSubject.php
+++ src/main/php/TypeSafe/security/DefaultSubject.php
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-require_once('typesafe/session/Session.php');
+require_once('TypeSafe/session/Session.php');
 require_once('Subject.php');
 require_once('SecurityManager.php');
 
